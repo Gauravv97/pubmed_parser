0525999c3af96fea4eba9d7477eed0a8408af02c
GauravChaudhari <gaurav_chaudhari@github.com>
2023-04-22 13:06:01 +0530

    Update medline_parser.py
    Iterator Version

--- pubmed_parser/medline_parser.py
+++ pubmed_parser/medline_parser.py
@@ -3,6 +3,7 @@ Parsers for MEDLINE XML
 """
 import re
 import numpy as np
+import gzip
 from itertools import chain
 from collections import defaultdict
 from pubmed_parser.utils import read_xml, stringify_children, month_or_day_formater
